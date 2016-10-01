806322e6ff5223e451aba38aaf281a65e1f75316
Douglas Bentley <Douglas Bentley>
2016-10-01 12:47:11 +0200

    Learned about variables

--- scripts/main.js
+++ scripts/main.js
@@ -7,17 +7,24 @@ scene = new THREE.Scene();
 camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 1, 10000 );
 camera.position.z = 1000;
 
-var geom1 = new THREE.CylinderGeometry(500, 500, 200, 6);
+var sides = 6;
+var width = 400;
+var height = 200;
+var gap = 20;
+
+var geom1 = new THREE.CylinderGeometry(width, width, height, sides);
 var mat1 = new THREE.MeshBasicMaterial( { color: 0xff0000, wireframe: true } );
 var mesh1 = new THREE.Mesh( geom1, mat1 );
-mesh1.position.y += 220;
-var geom2 = new THREE.CylinderGeometry(500, 500, 200, 6);
+mesh1.position.y += height + gap;
+
+var geom2 = new THREE.CylinderGeometry(width, width, height, sides);
 var mat2 = new THREE.MeshBasicMaterial( { color: 0x00ff00, wireframe: true } );
 var mesh2 = new THREE.Mesh( geom2, mat2 );
-var geom3 = new THREE.CylinderGeometry(500, 500, 200, 6);
+
+var geom3 = new THREE.CylinderGeometry(width, width, height, sides);
 var mat3 = new THREE.MeshBasicMaterial( { color: 0x0000ff, wireframe: true } );
 var mesh3 = new THREE.Mesh( geom3, mat3 );
-mesh3.position.y -= 220;
+mesh3.position.y -= height + gap;
 
 scene.add( mesh1 );
 scene.add( mesh2 );

8bfb098e739a496b3e3c5ad8794c34fe38f95a75
Douglas Bentley <Douglas Bentley>
2016-10-01 22:19:56 +0200

    Messing with aesthetics for no reason...
    because I don't even have gameplay yet so I should stahp

--- scripts/main.js
+++ scripts/main.js
@@ -8,32 +8,35 @@ scene = new THREE.Scene();
 camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 1, 10000 );
 camera.position.z = 1000;
 
-var touchcolor = 0xcccccc;
-var nontouchcolor = 0;
-var current = nontouchcolor;
-
 var sides = 6;
 var width = 350;
 var height = 200;
 var gap = 20;
 
+
 var geom1 = new THREE.CylinderGeometry(width, width, height, sides);
-var mat1 = new THREE.MeshBasicMaterial( { color: 0xee0000, wireframe: true } );
+var mat1 = new THREE.MeshPhongMaterial( { color: 0xee0000 } );
 var mesh1 = new THREE.Mesh( geom1, mat1 );
 mesh1.position.y += height + gap;
 
 var geom2 = new THREE.CylinderGeometry(width, width, height, sides);
-var mat2 = new THREE.MeshBasicMaterial( { color: 0x00ee00, wireframe: true } );
+var mat2 = new THREE.MeshPhongMaterial( { color: 0x00ee00 } );
 var mesh2 = new THREE.Mesh( geom2, mat2 );
 
 var geom3 = new THREE.CylinderGeometry(width, width, height, sides);
-var mat3 = new THREE.MeshBasicMaterial( { color: 0x0000ee, wireframe: true } );
+var mat3 = new THREE.MeshPhongMaterial( { color: 0x0000ee } );
 var mesh3 = new THREE.Mesh( geom3, mat3 );
 mesh3.position.y -= height + gap;
+
+var directionalLight = new THREE.DirectionalLight( 0xffffff, 0.6 );
+directionalLight.position.set( 0, 1, 10 );
+
+var current_shape = mesh3;
 
 scene.add( mesh1 );
 scene.add( mesh2 );
 scene.add( mesh3 );
+scene.add( directionalLight );
 
 renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
@@ -59,18 +62,20 @@ function handleTouchStart(e) {
     console.log(intersects);
     
     if (intersects.length > 0) {
-        current = intersects[0].object.material.color.getHex();
+        current_shape = intersects[0];
+        current_shape.object.material.wireframe = true;
     }
 }
 
 function handleTouchEnd(e) {
-    if (e.changedTouches[0].identifier === 0)
-        current = nontouchcolor;
+    //until I know what I want to do with multiple touches let's pretend others don't exist :)
+    if (e.changedTouches[0].identifier === 0) {
+        current_shape.object.material.wireframe = false;      
+    }
 }
 
 (function animate() {
     requestAnimationFrame( animate );
-    renderer.setClearColor ( current, 1);
 
     mesh1.rotation.y += 0.02;
     mesh2.rotation.y -= 0.02;
